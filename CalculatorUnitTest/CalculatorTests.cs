bbb0a8f02c087c8917eed4ac2d68ccc308c60be4
Nathan Shuai <nathan.shuai@gmail.com>
2023-10-06 11:28:44 -0500

    add other two method avoiding magic string

--- CalculatorUnitTest/CalculatorTests.cs
+++ CalculatorUnitTest/CalculatorTests.cs
@@ -48,9 +48,45 @@ namespace CalculatorUnitTest
 
             // Act
             calculator.PerformCalculation();
-
+            const string Result_Message = "Error: Division by zero";
             // Assert
-            Assert.AreEqual("Error: Division by zero", calculator.GetWarningLabelText());
+            Assert.AreEqual(Result_Message, calculator.GetWarningLabelText());
+        }
+
+        [TestMethod]
+
+        public void GetBinaryDisplay_ConvertTheNumberToBinary_ReturnBinaryRepresentation()
+        {
+            //Arrange 
+
+            CalculatorUnit calculator = new CalculatorUnit();
+
+            calculator.EnterNumber('7');
+
+            //Act
+            string binaryRepresentation = "00000111";
+           calculator.GetBinaryDisplay();
+
+            //Assert
+            Assert.AreEqual(binaryRepresentation, calculator.GetBinaryDisplay());
+
+        }
+        [TestMethod]
+        public void GetHexadecimalDisplay_ConvertTheNumberToHexadecimal_ReturnHexadecimalRepresentation()
+        {
+            //Arrange 
+
+            CalculatorUnit calculator = new CalculatorUnit();
+
+            calculator.EnterNumber('7');
+
+            //Act
+            string hexadecimalRepresentation = "00000007";
+            calculator.GetHexadecimalDisplay();
+
+            //Assert
+            Assert.AreEqual(hexadecimalRepresentation, calculator.GetHexadecimalDisplay());
+
         }
 
     }
